a2b393a3c3fe436e0357e0cce808d5b29284551a
AckermanFernando <fernandoloiola03@gmail..com>
2022-04-01 16:21:29 -0300

    remoção do campo que não estava sendo utilizado

---
+++ src/components/Tarefa.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card">
+    <header class="card-header is-align-items-center">
+      <div class="card-header-title column is-two-fifths">
+        {{ titulo }}
+      </div>
+      <!-- <div class="card-header-title column is-one-quarter">
+        outra coisa
+      </div> -->
+      <div class="card-header-title is-one-quarter is-justify-content-end">
+        <Cronometro :tempoEmSegundos="this.tempo"/>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue"
+// import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: {
+    Cronometro
+    // Temporizador,
+  },
+  props:{
+      titulo:{
+          type: String,
+          default: ''
+      },
+      tempo: {
+          type: Number,
+          default: 0
+      }
+  },
+// '   data() {
+//      return {
+//        titulo1: this.titulo,
+//      };
+//    },'
+//   methods:{
+//       criarTarefa(titulo: string, tempo: number){
+//         const novaTarefa = new Temporizador(titulo, tempo)
+//       }
+//   }
+});
+</script>
+
+<style scoped>
+
+</style>
